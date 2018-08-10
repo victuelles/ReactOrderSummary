1c8172902f8bd6af05f85bdade58e5d276d0de11
Rom Vic <technoyguy@gmail.com>
2018-08-10 14:50:04 -0700

    Initial setup and package update

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+       <h1>Walmart order summary </h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
