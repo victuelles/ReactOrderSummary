0b8158c730edcfc8fe9446fffa67841bcfef9268
Rom Vic <technoyguy@gmail.com>
2018-08-10 22:22:24 -0700

    promo code function

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import { Segment,Grid, Input,Button,Container,Popup} from 'semantic-ui-react'
+import { Segment,Grid,Button,Container,Popup,Form} from 'semantic-ui-react'
 import './App.css';
 import ItemList from './ItemList'
 //TO DO:
-// 1. create a json file to use as data for state
-// 2. move item details to a new component for reuse
-// 3. Implement redux to MATCH promo code
+// 1. Implement redux to MATCH promo code
 //    user should be able to enter a promo code “DISCOUNT” and on applying it, see a 10% discount reflect in the purchase summary. Use Redux for this
-// 4. List item details can have multiple items
 
 
-const items = [
+
+const data = {
+  subtotal:102.96,
+  pickup_savings:-3.85,
+  taxes:8.92,
+  zipcode:94587,
+  items:[
   {
     id: '1',
     title: 'Essentials by OFM',
@@ -30,15 +33,24 @@ const items = [
     retail_price: '29.98',
     quantity:'1',
     photoURL: 'https://i5.walmartimages.com/asr/7a112091-197a-48d9-9e58-6503ee0041c2_1.ee22dbd743135855c15fa10d50f5ec7a.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF'
-  }
-];
+  }]
+};
 
 class App extends Component {
   state = {
-    items: items,
+
+    items: data.items,
+    subtotal:data.subtotal,
+    pickup_savings:data.pickup_savings,
+    taxes:data.taxes,
+    discount:0,
+    estimated_total:data.subtotal+data.pickup_savings+data.taxes,
+    zipcode:data.zipcode,
     showItemDetails: false,
     showPromoCode: false,
-    itemPhotoURL:'https://i5.walmartimages.com/asr/e73e1252-642c-4473-93ea-fd3b564a7027_1.3e81ea58fa3042452fe185129a4a865f.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF'
+    promo_code:'',
+    promo_code_accepted: false,
+    
   }
   showItemDetailsToggle = () => {
     this.setState(prevState => ({
@@ -51,10 +63,46 @@ class App extends Component {
     }))
   }
 
+  onFormSubmit = (evt) => {
+    evt.preventDefault();
+    console.log("onFormSubmit")
+
+    // check for discount
+    if(this.state.promo_code==="DISCOUNT"){
+
+      const discount=this.state.estimated_total*0.1
+      const estimated_total=+((data.subtotal+data.pickup_savings+data.taxes-discount).toFixed(2))
+      this.setState({
+        estimated_total:estimated_total,
+        discount:discount,
+        promo_code_accepted:true,
+        showPromoCode: false
+      })
+
+    }else{
+      this.setState({
+        estimated_total:data.subtotal+data.pickup_savings+data.taxes,
+        promo_code_accepted:false,
+      })
+    }
+ 
+  }
+
+  onInputChange = (evt) => {
+    const newCode =  evt.target.value
+    
+    this.setState({
+      promo_code: newCode
+    })
+    console.log(this.state.promo_code)
+  }
+
   render() {
     const itemDetailsSign=!this.state.showItemDetails?'icon add':'icon minus'
     const promoCodeSign=!this.state.showPromoCode?'icon add':'icon minus'
-    console.log(itemDetailsSign);
+    let {subtotal,pickup_savings,taxes,zipcode,estimated_total,promo_code_accepted}=this.state;
+    const hasSaving=pickup_savings<0;
+   // console.log(subtotal);
     return (
       <React.Fragment>
         <Container className="main">
@@ -66,7 +114,7 @@ class App extends Component {
                   <p> Subtotal</p>
                 </Grid.Column>
                 <Grid.Column  textAlign='right'  >
-                      <strong>$102.96</strong>
+                      <strong>${subtotal}</strong>
                 </Grid.Column>
                 <Grid.Column textAlign='left' >
 
@@ -80,15 +128,20 @@ class App extends Component {
                 
                 </Grid.Column>
                 <Grid.Column  textAlign='right'  >
-                    <div style={{color:'red'}}> <strong >-$3.85</strong></div>
+                  {hasSaving && 
+                      <div style={{color:'red'}}> <strong >{pickup_savings}</strong></div>
+                  }
+                  {!hasSaving && 
+                      <div > <strong >{pickup_savings}</strong></div>
+                  }
                 </Grid.Column>
                 <Grid.Column>
                   <p> 
                     Est. Taxes and Fees<br/>
-                  (Based on 94587)</p>
+                  (Based on {zipcode})</p>
                 </Grid.Column>
                 <Grid.Column  textAlign='right'  >
-                      <strong>$8.92</strong>
+                      <strong>{taxes}</strong>
                 </Grid.Column>
               </Grid.Row>
 
@@ -99,7 +152,8 @@ class App extends Component {
                   </Grid.Column>
                   <Grid.Column  textAlign='right'  >
                   <div className="ui header"> 
-                    $108.03</div>
+                   {estimated_total}</div>
+                   {promo_code_accepted&& <div className="ui">(10% discount)</div>}
                   </Grid.Column>
                   <Grid.Column>
                       <a onClick={this.showItemDetailsToggle}> 
@@ -124,16 +178,20 @@ class App extends Component {
               </Grid.Column>
               {this.state.showPromoCode &&
                   <Grid.Column>
-                    <div className="ui sub header">Promo Code
-                    </div>
-                    <Input focus/>
-                    <Button basic color='black'>Apply</Button>
+                    <div className="ui sub header">Promo Code </div>
+                    <Form onSubmit={this.onFormSubmit}>
+                      <Form.Field>
+
+                        <input name='promo_code' onChange={this.onInputChange} value={this.state.promo_code} />
+                      </Form.Field>
+                      <Button basic color='black' type="submit">Apply</Button>
+                    </Form>
+                   
+                    
                   </Grid.Column>
               }
      
               </Grid.Row>
-
-
             </Grid>
           </Segment>
         </Container>
